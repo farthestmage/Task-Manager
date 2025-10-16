43307cad13d17db1ac54eb5fa20e54e2af65f2c1
farthestmage <arnavkamboj511@gmail.com>
2025-10-16 16:36:07 +0530

    Update- Task

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import typer
-from model import Task
+from model import Task,datetime
 from data import read,write
-
+from erros import TaskNotFoundError
 
 
 
@@ -21,6 +21,14 @@ def write_json_data(new_task : Task,task_dict:dict={}):
                                       "updatedAt":new_task.updatedAt}
     write(data=task_dict)
     task_dict= read_json_data()
+def update_json_data(id:int ,task: Task,task_dict):
+    task_dict[id-1]={"task_name":task.task_name,
+                "description":task.description,
+                "status":task.status,
+                "createdAt":task.createdAt,
+                "updatedAt":task.updatedAt}
+    write(data=task_dict)
+    task_dict= read_json_data()
 
 task_dict = read_json_data()
 
@@ -37,6 +45,26 @@ def add(task_name: str):
     if not task_dup:
         print(f"Added {task_name} Task_ID: {len(task_dict.keys())+1}")
         write_json_data(task_add,task_dict if len(task_dict)>0 else {})
+
+@app.command()
+def update(id:int,task_name:str):
+   try: 
+        for id_task,task in task_dict.items():
+            if ((id-1) ==int(id_task)):
+                
+                temp_t=Task(**task)
+                temp_t.task_name = task_name
+                temp_t.updatedAt =str(datetime.now())
+                
+                update_json_data(id,temp_t,task_dict)
+                print("UPDATED")
+                return
+        raise TaskNotFoundError("Task ID not Found")
+                
+   except TaskNotFoundError as e:
+       print(e)
+
+
 
 @app.command()
 def goodbye(name: str, formal: bool = False):
