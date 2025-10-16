ae0da005e8abcd94baa7747ea2bde29a0fd5aca8
farthestmage <arnavkamboj511@gmail.com>
2025-10-16 13:58:25 +0530

    Task Model

--- model.py
+++ model.py
@@ -9,13 +9,6 @@ class Task:
         self.createdAt=str(createdAt)
         self.updatedAt= str(updatedAt)
 
-    '''  def add(self,task_name,description="Add Description",status="todo",createdAt=datetime.now(),updatedAt=datetime.now()):
-        self.task_name = task_name
-        self.description = description
-        self.status = status
-        self.createdAt=str(createdAt)
-        self.updatedAt= str(updatedAt)'''
-
     def __str__(self):
        return f" {self.task_name} {self.status} \n   {self.description}"
         
